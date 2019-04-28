b3bacbffe422f3c1d4a4797203e669d723b2ed97
NotCompsky <adammarkgray@gmail.com>
2019-04-28 17:02:06 +0100

    Replaced colour rtagger with colour rtags for each category of tag (but neither category names nor subreddit names are written).
    SQL: Added SQL examples.

--- src/mysql__rtagged.cpp
+++ src/mysql__rtagged.cpp
@@ -18,22 +18,27 @@ sql::ResultSet* SQL_RES;
 
 
 constexpr const char* STMT_PRE = 
-    "SELECT S2T.user_id, SUM(S2T.c), SUM(S2T.c*t.r), SUM(S2T.c*t.g), SUM(S2T.c*t.b) "
-    "FROM tag t "
-    "JOIN ("
-        "SELECT U2SCC.user_id, s2t.tag_id, SUM(U2SCC.count) as c "
-        "FROM subreddit2tag s2t "
-        "JOIN ("
-            "SELECT u2scc.user_id, u2scc.subreddit_id, u2scc.count "
-            "FROM user2subreddit_cmnt_count u2scc "
-            "WHERE u2scc.user_id IN (";
-    
+    "SELECT A.user_id, SUM(A.c), SUM(A.r), SUM(A.g), SUM(A.b) "
+    "FROM tag2category t2c "
+    "JOIN ( "
+        "SELECT S2T.user_id, S2T.tag_id, S2T.c, S2T.c*t.r as r, S2T.c*t.g as g, S2T.c*t.b as b "
+        "FROM tag t "
+        "JOIN ( "
+            "SELECT U2SCC.user_id, s2t.tag_id, SUM(U2SCC.count) as c "
+            "FROM subreddit2tag s2t "
+            "JOIN ( "
+                "SELECT u2scc.user_id, u2scc.subreddit_id, u2scc.count "
+                "FROM user2subreddit_cmnt_count u2scc "
+                "WHERE u2scc.user_id IN (";
+
 constexpr const char* STMT_POST = 
-        ")) U2SCC ON U2SCC.subreddit_id = s2t.subreddit_id "
-        "GROUP BY U2SCC.user_id, s2t.tag_id"
-    ") S2T ON S2T.tag_id = t.id "
-    "WHERE t.r IS NOT NULL "
-    "GROUP BY S2T.user_id;";
+            ")) U2SCC ON U2SCC.subreddit_id = s2t.subreddit_id "
+            "GROUP BY U2SCC.user_id, s2t.tag_id "
+        ") S2T ON S2T.tag_id = t.id "
+        "WHERE t.r IS NOT NULL "
+    ") A ON t2c.tag_id = A.tag_id "
+    "GROUP BY A.user_id "
+    ";";
 
 constexpr const char* id_t2_ = "id-t2_";
 
@@ -91,15 +96,22 @@ void write_cl_channel(char* dst,  const int n_cmnts,  float x){
 }
 
 
+char* DST;
+
 extern "C"
-int n_required_bytes(char* csv){
+void free_dst(){
+    free(DST-1); // Since we added 1 to it before
+}
+
+int estimated_n_bytes(const char* csv){
     int n = 0;
+    int i = 0;
     while (true){
-        if (*csv == ',')
-            n += 5 + 11; // 5 for "":""  11 for rgb(123456)
-        else if (*csv == 0)
+        if (csv[i] == ',')
+            n += 2 + 14*2; // 2 for "": minues ,  14 for each "rgb(123456)",
+        else if (csv[i] == 0)
             return 1 + (n+5+11) + 1 + 1; // { ... }\0
-        ++csv;
+        ++i;
         ++n;
     }
 }
@@ -114,7 +126,7 @@ void init_mysql(const char* mysql_url,  const char* mysql_usr,  const char* mysq
 }
 
 extern "C"
-void csv2cls(const char* csv,  char* dst){
+void csv2cls(const char* csv){
     /*
     The input id-t2_IDSTR,id-t2_IDSTR2,id-t2_IDSTR3, ... maps to {"IDSTR":"#0123456", ... }
     
@@ -165,36 +177,54 @@ void csv2cls(const char* csv,  char* dst){
     SQL_RES = SQL_STMT->executeQuery(stmt);
     
     int k = 0;
-    dst[k++] = '{';
-    bool any_results = false;
+    int n_allocated_bytes = estimated_n_bytes(csv);
+    DST = (char*)malloc(n_allocated_bytes);
+    //DST[k++] = '{'; We obtain an (erroneous) prefix of "]," in the following loop
+    // To avoid adding another branch, we simply skip the first character, and overwrite the second with "{" later
+    uint64_t last_id = 0;
     while (SQL_RES->next()){
-        any_results = true;
         const uint64_t id = SQL_RES->getUInt64(1);
         const int n_cmnts = SQL_RES->getInt(2);
         
-        dst[k++] = '"';
-        memcpy(dst + k,  id_t2_,  strlen(id_t2_));
-        k += strlen(id_t2_);
-        k += id2str(id,  dst + k); // Write ID as alphanumeric string
-        dst[k++] = '"';
-        dst[k++] = ':';
-        dst[k++] = '"';
-        dst[k++] = 'r';
-        dst[k++] = 'g';
-        dst[k++] = 'b';
-        dst[k++] = '(';
-        for (auto c = 3;  c < 6;  ++c){
-            k += itoa_nonstandard((unsigned char)(255*SQL_RES->getDouble(c)),  dst + k);
-            dst[k++] = ',';
+        if (k <= n_allocated_bytes - strlen("],\"id-t2_abcdefgh\":[\"rgb(255,255,255)\",") - 1){
+            // -1 for terminating null byte
+            n_allocated_bytes = (n_allocated_bytes * 3) / 2;
+            n_allocated_bytes += strlen("],\"id-t2_abcdefgh\":[\"rgb(255,255,255)\",") + 1;
+            DST = (char*)realloc(DST, n_allocated_bytes);
         }
-        --k; // Clear trailing comma
-        dst[k++] = ')';
-        dst[k++] = '"';
-        dst[k++] = ',';
+        
+        if (id != last_id){
+            DST[k] = ']'; // Overwrite trailing comma left by RGBs
+            DST[++k] = ',';
+            DST[++k] = '"';
+            memcpy(DST + k + 1,  id_t2_,  strlen(id_t2_));
+            k += strlen(id_t2_);
+            k += id2str(id,  DST + k + 1); // Write ID as alphanumeric string
+            DST[++k] = '"';
+            DST[++k] = ':';
+            DST[++k] = '[';
+        }
+        DST[++k] = '"';
+        DST[++k] = 'r';
+        DST[++k] = 'g';
+        DST[++k] = 'b';
+        DST[++k] = '(';
+        for (auto c = 3;  c < 6;  ++c){
+            k += itoa_nonstandard((unsigned char)(255*SQL_RES->getDouble(c)),  DST + k + 1);
+            DST[++k] = ',';
+        }
+        DST[k] = ')'; // Clear trailing comma
+        DST[++k] = '"';
+        DST[++k] = ',';
+        
+        DST[k+1] = 0;
     }
-    if (any_results)
+    if (k != 0){
+        DST[k] = ']';
         --k; // Clear trailing comma
-    dst[k++] = '}';
-    dst[k] = 0;
-    printf("%s\n", dst);
+        ++DST; // Skip first character
+    }
+    DST[0] = '{';
+    DST[++k] = '}';
+    DST[++k] = 0;
 }
--- wrapper.py
+++ wrapper.py
@@ -6,13 +6,14 @@ import ctypes
 
 # One query
 def query(userids:bytes):
-    n = rtagged.n_required_bytes(userids)
-    s = ctypes.create_string_buffer(n + 1)
-    rtagged.csv2cls(userids, s)
+    rtagged.csv2cls(userids)
+    s:bytes = ctypes.c_char_p.in_dll(rtagged, "DST")
     print(s.value)
+    rtagged.free_dst() # Must be called after every call to csv2cls
 
 
-def main():
+if __name__ == "__main__":
     rtagged = ctypes.cdll.LoadLibrary("build/rtagged.so")
     rtagged.init_mysql(b"unix:///var/run/mysqld/mysqld.sock", b"rscraper++", b"PASSWRD")
-    userids:str = b"id-t2_6l4z3,id-t2_32duad4"
+    
+    query(b"id-t2_6l4z3,id-t2_32duad4")
