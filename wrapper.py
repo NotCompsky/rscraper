2d4bd2b2cc1aa6bbf3fdffb9cbcdfbb1e31fa5b3
NotCompsky <adammarkgray@gmail.com>
2019-06-10 10:36:28 +0100

    Updated

--- wrapper.py
+++ wrapper.py
@@ -9,11 +9,10 @@ def query(userids:bytes):
     rtagged.csv2cls(userids)
     s:bytes = ctypes.c_char_p.in_dll(rtagged, "DST")
     print(s.value)
-    rtagged.free_dst() # Must be called after every call to csv2cls
 
 
 if __name__ == "__main__":
-    rtagged = ctypes.cdll.LoadLibrary("build/rtagged.so")
-    rtagged.init_mysql(b"unix:///var/run/mysqld/mysqld.sock", b"rscraper++", b"PASSWRD")
+    rtagged = ctypes.cdll.LoadLibrary("librscraper-tagger.so")
+    rtagged.init()
     
     query(b"id-t2_6l4z3,id-t2_32duad4")
