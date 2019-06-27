07b88a4e92e37c466bf9b82f91996f72a209c63f
NotCompsky <adammarkgray@gmail.com>
2019-06-27 08:15:31 +0100

    Check fopen return code for failure (as in, no file found).
    Optimised importing of subreddit2tag.

--- io/src/import.cpp
+++ io/src/import.cpp
@@ -15,6 +15,14 @@
 MYSQL_RES* RES;
 MYSQL_ROW ROW;
 
+bool force = false;
+
+char str_a[1024];
+char str_b[1024];
+char uint64_a[20]; // 19 digits + 1 null byte
+char uint64_b[20];
+char uint64_c[20];
+
 
 constexpr size_t strlen_constexpr(const char* s){
     // GCC strlen is constexpr; this is apparently a bug
@@ -27,9 +35,10 @@ namespace compsky {
         constexpr static const size_t BUF_SZ = 4 * 1024 * 1024;
         char* BUF = (char*)malloc(4 * 1024 * 1024); // 4 MiB
             
-        void ensure_buf_can_fit(const char* pre,  size_t n){
+        void ensure_buf_can_fit(const char* pre,  size_t n,  size_t trim_trailing_bytes = 1){
             if (unlikely(BUF_INDX + n  >  BUF_SZ)){
-                compsky::mysql::exec_buffer(BUF,  BUF_INDX - 1); // Ignore trailing comma
+                compsky::mysql::exec_buffer(BUF,  BUF_INDX - trim_trailing_bytes);
+                // Usually a trailing comma
                 BUF_INDX = 0;
                 asciify(pre);
             }
@@ -60,22 +69,130 @@ bool contains(const char** ls,  const int n,  const char* s){
     return false;
 }
 
+void import_user_table(FILE* f){
+    compsky::asciify::BUF_INDX = 0;
+    constexpr static const char* pre = "INSERT IGNORE INTO user (id,name) VALUES ";
+    compsky::asciify::asciify(pre);
+    while(fscanf(f, "%[^\t\n]\t%[^\n]\n", uint64_a, str_a) != EOF){
+        compsky::asciify::asciify('(', uint64_a, ',', '"', _f::esc, '"', str_a, '"', ')', ',');
+        compsky::asciify::ensure_buf_can_fit(pre,  1 + 19 + 1 + 1 + 1 + 2*128 + 1 + 1 + 1);
+    }
+    if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
+        compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
+    fclose(f);
+}
+
+void import_subreddit_table(FILE* f){
+    compsky::asciify::BUF_INDX = 0;
+    constexpr static const char* pre = "INSERT IGNORE INTO subreddit (id,name) VALUES ";
+    compsky::asciify::asciify(pre);
+    while(fscanf(f, "%[^\t\n]\t%[^\n]\n", uint64_a, str_a) != EOF){
+        compsky::asciify::asciify('(', uint64_a, ',', '"', _f::esc, '"', str_a, '"', ')', ',');
+        compsky::asciify::ensure_buf_can_fit(pre,  1 + 19 + 1 + 1 + 1 + 2*128 + 1 + 1 + 1);
+    }
+    if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
+        compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
+    fclose(f);
+}
+
+void import_tag_table(FILE* f){
+    compsky::asciify::BUF_INDX = 0;
+    constexpr static const char* pre = "INSERT IGNORE INTO tag (name, r, g, b, a) VALUES ";
+    compsky::asciify::asciify(pre);
+    while(fscanf(f, "%[^\t\n]\t%[^\n]\n", str_a, str_b) != EOF){
+        // `str_b` itself is of the format %lf,%lf,%lf,%lf
+        compsky::asciify::asciify("(\"", _f::esc, '"', str_a, "\",", str_b, "),");
+        compsky::asciify::ensure_buf_can_fit(pre,  2 + 2*128 + 2 + 4*7 + 2);
+    }
+    if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
+        compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
+    fclose(f);
+}
+
+void import_category_table(FILE* f){
+    compsky::asciify::BUF_INDX = 0;
+    constexpr static const char* pre = "INSERT IGNORE INTO category (name) VALUES ";
+    compsky::asciify::asciify(pre);
+    while(fscanf(f, "%[^\n]\n", str_a) != EOF){
+        compsky::asciify::asciify("(\"", _f::esc, '"', str_a, "\"),");
+        compsky::asciify::ensure_buf_can_fit(pre,  2 + 2*128 + 3);
+    }
+    if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
+        compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
+    fclose(f);
+}
+
+void import_u2scc_table(FILE* f){
+    compsky::asciify::BUF_INDX = 0;
+    char* pre;
+    char* post;
+    size_t post_strlen;
+    if (force){
+        pre  = "INSERT INTO user2subreddit_cmnt_count (user_id,subreddit_id,count) VALUES ";
+        post = " ON DUPLICATE KEY SET count=VALUES(count)";
+        post_strlen = strlen_constexpr("INSERT INTO user2subreddit_cmnt_count (user_id,subreddit_id,count) VALUES ");
+    } else {
+        pre = "INSERT IGNORE INTO user2subreddit_cmnt_count (user_id,subreddit_id,count) VALUES ";
+        post = " ";
+        post_strlen = 0;
+    }
+    compsky::asciify::asciify(pre);
+    while(fscanf(f, "%[^\t\n]\t%[^\t\n]\t%[^\n]\n", uint64_a, uint64_b, uint64_c) != EOF){
+        compsky::asciify::asciify("(", uint64_a, ',', uint64_b, ',', uint64_c, "),");
+        compsky::asciify::ensure_buf_can_fit(pre,  2 + 2*128 + 3,  post, post_strlen);
+    }
+    if (compsky::asciify::BUF_INDX != strlen(pre))
+        compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
+    fclose(f);
+}
+
+void import_subreddit2tag_table(FILE* f){
+    compsky::asciify::BUF_INDX = 0;
+    constexpr static const char* pre = "INSERT IGNORE INTO subreddit2tag (subreddit_id,tag_id) SELECT s.id,t.id FROM subreddit s, tag t WHERE ";
+    compsky::asciify::asciify(pre);
+    while(fscanf(f, "%[^\t\n]\t%[^\n]\n", str_a, str_b) != EOF){
+        compsky::asciify::asciify("(s.name=\"", _f::esc, '"', str_a, "\" AND t.name=\"", _f::esc, '"', str_b, "\") OR ");
+        compsky::asciify::ensure_buf_can_fit(pre,  9 + 2*128 + 14 + 2*128 + 6,  4);
+        // 6 being strlen_constexpr(" OR ")
+    }
+    if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
+        compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 4);
+    fclose(f);
+}
+
+void import_tag2category_table(FILE* f){
+    while(fscanf(f, "%[^\t\n]\t%[^\n]\n", str_a, str_b) != EOF)
+        compsky::mysql::exec("INSERT IGNORE INTO tag2category (tag_id,category_id) SELECT t.id,c.id FROM tag t, category c WHERE t.name=\"", _f::esc, '"', str_a, "\" AND c.name=\"", _f::esc, '"', str_b, "\"");
+    fclose(f);
+}
+
+void import_subredditsbasic_table(FILE* f){
+    compsky::asciify::BUF_INDX = 0;
+    constexpr static const char* pre = "INSERT IGNORE INTO subreddit2meta (id,subscribers,created_at) VALUES ";
+    compsky::asciify::asciify(pre);
+    while(fscanf(f, "%[^,],%[^,],%[^,],%[^,],%[^,\n]\n", uint64_a, uint64_b, uint64_c, str_a, str_b) != EOF){
+        // Note that str_b stores 'subscribers' column, so only needs 20 char
+        if (str_b[0] == 'N')
+            // == None
+            continue;
+        compsky::asciify::asciify("(", uint64_a, ',', str_b, ',', uint64_c, "),");
+        compsky::asciify::ensure_buf_can_fit(pre,  1 + 19 + 1 + 19 + 1 + 19 + 2);
+    }
+    if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
+        compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
+    fclose(f);
+}
+
 int main(int argc,  const char** argv){
     compsky::mysql::init(getenv("RSCRAPER_MYSQL_CFG"));  // Init SQL
     
     FILE* f;
-    char str_a[1024];
-    char str_b[1024];
-    char uint64_a[20]; // 19 digits + 1 null byte
-    char uint64_b[20];
-    char uint64_c[20];
     
     ++argv;
     --argc;
     
     
     // Arg Parser
-    bool force = false;
     while(argc != 0){
         const char* s = *argv;
         if (s[2] != 0  ||  s[0] != '-')
@@ -95,129 +212,44 @@ int main(int argc,  const char** argv){
     
     /* No dependencies on other tables */
     
-    if (argc == 0  ||  contains(argv, argc, "user.csv")){
-        f = fopen("user.csv", "rb");
-        compsky::asciify::BUF_INDX = 0;
-        constexpr static const char* pre = "INSERT IGNORE INTO user (id,name) VALUES ";
-        compsky::asciify::asciify(pre);
-        while(fscanf(f, "%[^\t\n]\t%[^\n]\n", uint64_a, str_a) != EOF){
-            compsky::asciify::asciify('(', uint64_a, ',', '"', _f::esc, '"', str_a, '"', ')', ',');
-            compsky::asciify::ensure_buf_can_fit(pre,  1 + 19 + 1 + 1 + 1 + 2*128 + 1 + 1 + 1);
-        }
-        if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
-            compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
-        fclose(f);
-    }
+    if (argc == 0  ||  contains(argv, argc, "user.csv"))
+        if ((f = fopen("user.csv", "rb")))
+            import_user_table(f);
     
-    if (argc == 0  ||  contains(argv, argc, "subreddit.csv")){
-        f = fopen("subreddit.csv", "rb");
-        compsky::asciify::BUF_INDX = 0;
-        constexpr static const char* pre = "INSERT IGNORE INTO subreddit (id,name) VALUES ";
-        compsky::asciify::asciify(pre);
-        while(fscanf(f, "%[^\t\n]\t%[^\n]\n", uint64_a, str_a) != EOF){
-            compsky::asciify::asciify('(', uint64_a, ',', '"', _f::esc, '"', str_a, '"', ')', ',');
-            compsky::asciify::ensure_buf_can_fit(pre,  1 + 19 + 1 + 1 + 1 + 2*128 + 1 + 1 + 1);
-        }
-        if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
-            compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
-        fclose(f);
-    }
+    if (argc == 0  ||  contains(argv, argc, "subreddit.csv"))
+        if ((f = fopen("subreddit.csv", "rb")))
+            import_subreddit_table(f);
     
-    if (argc == 0  ||  contains(argv, argc, "tag.csv")){
-        double r, g, b, a;
-        f = fopen("tag.csv", "rb");
-        compsky::asciify::BUF_INDX = 0;
-        constexpr static const char* pre = "INSERT IGNORE INTO tag (name, r, g, b, a) VALUES ";
-        compsky::asciify::asciify(pre);
-        while(fscanf(f, "%[^\t\n]\t%[^\n]\n", str_a, str_b) != EOF){
-            // `str_b` itself is of the format %lf,%lf,%lf,%lf
-            compsky::asciify::asciify("(\"", _f::esc, '"', str_a, "\",", str_b, "),");
-            compsky::asciify::ensure_buf_can_fit(pre,  2 + 2*128 + 2 + 4*7 + 2);
-        }
-        if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
-            compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
-        fclose(f);
-    }
+    if (argc == 0  ||  contains(argv, argc, "tag.csv"))
+        if ((f = fopen("tag.csv", "rb")))
+            import_tag_table(f);
     
-    if (argc == 0  ||  contains(argv, argc, "category.csv")){
-        double r, g, b, a;
-        f = fopen("category.csv", "rb");
-        compsky::asciify::BUF_INDX = 0;
-        constexpr static const char* pre = "INSERT IGNORE INTO category (name) VALUES ";
-        compsky::asciify::asciify(pre);
-        while(fscanf(f, "%[^\n]\n", str_a) != EOF){
-            compsky::asciify::asciify("(\"", _f::esc, '"', str_a, "\"),");
-            compsky::asciify::ensure_buf_can_fit(pre,  2 + 2*128 + 3);
-        }
-        if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
-            compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
-        fclose(f);
-    }
+    if (argc == 0  ||  contains(argv, argc, "category.csv"))
+        if ((f = fopen("category.csv", "rb")))
+            import_category_table(f);
     
     /* No dependencies on previous import data (i.e. just using absolute IDs) */
     
-    if (argc == 0  ||  contains(argv, argc, "user2subreddit_cmnt_count.csv")){
-        f = fopen("user2subreddit_cmnt_count.csv", "rb");
-        compsky::asciify::BUF_INDX = 0;
-        char* pre;
-        char* post;
-        size_t post_strlen;
-        if (force){
-            pre  = "INSERT INTO user2subreddit_cmnt_count (user_id,subreddit_id,count) VALUES ";
-            post = " ON DUPLICATE KEY SET count=VALUES(count)";
-            post_strlen = strlen_constexpr("INSERT INTO user2subreddit_cmnt_count (user_id,subreddit_id,count) VALUES ");
-        } else {
-            pre = "INSERT IGNORE INTO user2subreddit_cmnt_count (user_id,subreddit_id,count) VALUES ";
-            post = " ";
-            post_strlen = 0;
-        }
-        compsky::asciify::asciify(pre);
-        while(fscanf(f, "%[^\t\n]\t%[^\t\n]\t%[^\n]\n", uint64_a, uint64_b, uint64_c) != EOF){
-            compsky::asciify::asciify("(", uint64_a, ',', uint64_b, ',', uint64_c, "),");
-            compsky::asciify::ensure_buf_can_fit(pre,  2 + 2*128 + 3,  post, post_strlen);
-        }
-        if (compsky::asciify::BUF_INDX != strlen(pre))
-            compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
-        fclose(f);
-    }
+    if (argc == 0  ||  contains(argv, argc, "user2subreddit_cmnt_count.csv"))
+        if ((f = fopen("user2subreddit_cmnt_count.csv", "rb")))
+            import_u2scc_table(f);
     
     /* Name-to-name tables */
     
-    if (argc == 0  ||  contains(argv, argc, "subreddit2tag.csv")){
-        f = fopen("subreddit2tag.csv", "rb");
-        while(fscanf(f, "%[^\t\n]\t%[^\n]\n", str_a, str_b) != EOF){
-            compsky::mysql::exec("INSERT IGNORE INTO subreddit2tag (subreddit_id,tag_id) SELECT s.id,t.id FROM subreddit s, tag t WHERE s.name=\"", _f::esc, '"', str_a, "\" AND t.name=\"", _f::esc, '"', str_b, "\"");
-        }
-        fclose(f);
-    }
+    if (argc == 0  ||  contains(argv, argc, "subreddit2tag.csv"))
+        if ((f = fopen("subreddit2tag.csv", "rb")))
+            import_subreddit2tag_table(f);
     
-    if (argc == 0  ||  contains(argv, argc, "tag2category.csv")){
-        f = fopen("tag2category.csv", "rb");
-        while(fscanf(f, "%[^\t\n]\t%[^\n]\n", str_a, str_b) != EOF)
-            compsky::mysql::exec("INSERT IGNORE INTO tag2category (tag_id,category_id) SELECT t.id,c.id FROM tag t, category c WHERE t.name=\"", _f::esc, '"', str_a, "\" AND c.name=\"", _f::esc, '"', str_b, "\"");
-        fclose(f);
-    }
+    if (argc == 0  ||  contains(argv, argc, "tag2category.csv"))
+        if ((f = fopen("tag2category.csv", "rb")))
+            import_tag2category_table(f);
     
     /* 3rd party tables */
     
     // Specifically for importing https://files.pushshift.io/reddit/subreddits/subreddits_basic.csv
-    if (argc == 0  ||  contains(argv, argc, "subreddits_basic.csv")){
-        f = fopen("subreddits_basic.csv", "rb");
-        compsky::asciify::BUF_INDX = 0;
-        constexpr static const char* pre = "INSERT IGNORE INTO subreddit2meta (id,subscribers,created_at) VALUES ";
-        compsky::asciify::asciify(pre);
-        while(fscanf(f, "%[^,],%[^,],%[^,],%[^,],%[^,\n]\n", uint64_a, uint64_b, uint64_c, str_a, str_b) != EOF){
-            // Note that str_b stores 'subscribers' column, so only needs 20 char
-            if (str_b[0] == 'N')
-                // == None
-                continue;
-            compsky::asciify::asciify("(", uint64_a, ',', str_b, ',', uint64_c, "),");
-            compsky::asciify::ensure_buf_can_fit(pre,  1 + 19 + 1 + 19 + 1 + 19 + 2);
-        }
-        if (compsky::asciify::BUF_INDX != strlen_constexpr(pre))
-            compsky::mysql::exec_buffer(compsky::asciify::BUF,  compsky::asciify::BUF_INDX - 1); // Overwrite trailing comma
-        fclose(f);
-    }
+    if (argc == 0  ||  contains(argv, argc, "subreddits_basic.csv"))
+        if ((f = fopen("subreddits_basic.csv", "rb")))
+            import_subredditsbasic_table(f);
     
     compsky::mysql::exit_mysql();
 }
