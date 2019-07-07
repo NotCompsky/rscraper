79eb1d326c5e1a949a297d596818c9b11c1c5282
NotCompsky <adammarkgray@gmail.com>
2019-07-07 14:25:49 -0400

    Replace quotes with double quotes for strings

--- tagger/browser-addon/options.js
+++ tagger/browser-addon/options.js
@@ -1,15 +1,15 @@
 // Based on the example provided on https://developers.chrome.com/extensions/options
 
 function save_options() {
-    var url = document.getElementById('url').value;
+    var url = document.getElementById("url").value;
     chrome.storage.sync.set({
         url: url
     }, function() {
         // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
+        var status = document.getElementById("status");
+        status.textContent = "Options saved.";
         setTimeout(function() {
-            status.textContent = '';
+            status.textContent = "";
         }, 750);
     });
 }
@@ -19,8 +19,8 @@ function restore_options() {
   chrome.storage.sync.get({
     url: "http://104.197.15.19:8080/"
   }, function(items) {
-    document.getElementById('url').value = items.url;
+    document.getElementById("url").value = items.url;
   });
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
