fdd1e6353b624ab5a6ba6a2dd9b7265cada9bed2
NotCompsky <adammarkgray@gmail.com>
2019-07-11 18:56:47 -0400

    Tidy regex, and add highlighting for variable substitutions.

--- hub/src/regex_editor_highlighter.cpp
+++ hub/src/regex_editor_highlighter.cpp
@@ -6,18 +6,20 @@
 
 #define n_highlighting_rules 8
 static const QRegularExpression highlighting_regex(
-	"(?<!\\\\)(?:"
+	"(?<![^\\\\]\\\\)(?:"
 		"([(])([?]P<([^>]*)>)?|"	// Capture group opening bracket (#1), and optionally name (inner: #3, outer: #2)
-		"([)])"				// Capture group closing bracket (#4) // NOTE: [(] is a false positive; left and right brackets are not paired up
+		"([)])|"			// Capture group closing bracket (#4) // NOTE: [(] is a false positive; left and right brackets are not paired up
+		"(?<=^)([ \t]*)|"		// Whitespace after newline that is ignored by the hub's pre-processor (#5) // NOTE: Fails to not comment out lines preceded by an escape character - newlines work weirdly. // TODO: Fix this.
+		"(\\$\\{[^}]+\\})"		// Variable substitution (#6) (not implemented into regex pre-processor yet, but planned)
 	")|"
-	"((?:^|[ \t]+)#.*)|"			// Comment (#5)
-	"(\\\\[\\\\nrtv])|"			// Escape sequence parsed by the hub's pre-processor (#6) // TODO: maybe ignore if preceded by an odd number of escape characters
-	"(?<!\\\\)(?<=^)([ \t]*)"		// Whitespace after newline that is ignored by the hub's pre-processor (#7) // NOTE: Fails to not comment out lines preceded by an escape character - newlines work weirdly. // TODO: Fix this.
+	"((?:^|[ \t]+)#.*)|"			// Comment (#7)
+	"(\\\\[\\\\nrtv])"			// Escape sequence parsed by the hub's pre-processor (#8) // TODO: maybe ignore if preceded by an odd number of escape characters
 );
 
-static QTextCharFormat highlighting_fmts[n_highlighting_rules];
+static QTextCharFormat highlighting_fmts[n_highlighting_rules+1];
 
 static const QColor cl_comment(0, 255, 0, 70);
+static const QColor cl_varsub(0, 0, 255, 70);
 
 
 RegexEditorHighlighter::RegexEditorHighlighter(QTextDocument* parent)
@@ -25,14 +27,18 @@ RegexEditorHighlighter::RegexEditorHighlighter(QTextDocument* parent)
 {
     highlighting_fmts[1].setForeground(Qt::blue);	// Capture group opening bracket
     highlighting_fmts[1].setFontWeight(QFont::Bold);	// Capture group opening bracket
-    highlighting_fmts[2].setForeground(Qt::darkBlue);
-    highlighting_fmts[3].setForeground(Qt::darkBlue);
-    highlighting_fmts[3].setFontWeight(QFont::Bold);
+    highlighting_fmts[2].setForeground(Qt::darkBlue);	// Capture group name (outer)
+    highlighting_fmts[3].setForeground(Qt::darkBlue);	// Capture group name (inner)
+    highlighting_fmts[3].setFontWeight(QFont::Bold);	// Capture group name (inner)
     highlighting_fmts[4].setForeground(Qt::blue);	// Capture group closing bracket
     highlighting_fmts[4].setFontWeight(QFont::Bold);	// Capture group closing bracket
-    highlighting_fmts[5].setBackground(cl_comment);
-    highlighting_fmts[6].setForeground(Qt::red);
-    highlighting_fmts[7].setBackground(cl_comment);
+    highlighting_fmts[7].setBackground(cl_comment);	// Comment
+    highlighting_fmts[8].setForeground(Qt::red);	// Escape characters parsed by pre-processor
+    highlighting_fmts[5].setBackground(cl_comment);	// Comment
+    highlighting_fmts[6].setForeground(Qt::yellow);	// Variable substitution
+    highlighting_fmts[6].setBackground(cl_varsub);	// Variable substitution
+    highlighting_fmts[6].setFontWeight(QFont::Bold);	// Variable substitution
+    // NOTE: background highlights are used to indicate that the length of the resulting regex would be modified - either by removing whitespace, or pasting text with variable substitution.
 }
 
 void RegexEditorHighlighter::highlightBlock(const QString& text) {
