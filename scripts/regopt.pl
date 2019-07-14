e922c383588c1c692d21ba8c9dd4163ac30104b1
NotCompsky <adammarkgray@gmail.com>
2019-07-14 14:24:35 -0400

    Add comments

--- scripts/regopt.pl
+++ scripts/regopt.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/env perl
+
+# NOTE: Input must be valid regex, but some regex patterns cause the optimiser to skip the entire string. E.g. '(?:foo|bar)'. See https://bisqwit.iki.fi/source/regexopt.html for more.
+
 use Regexp::Optimizer;
 my $regexpr = @ARGV[0];
 print Regexp::Optimizer->new->optimize(qr/$regexpr/);
