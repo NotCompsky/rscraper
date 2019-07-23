3dbf7350f475180b4695e753fcc01e07a0203198
NotCompsky <adammarkgray@gmail.com>
2019-07-23 14:15:29 +0100

    Update comments

--- shared-src/init_regexp_from_file.cpp
+++ shared-src/init_regexp_from_file.cpp
@@ -17,9 +17,8 @@ void init_regexp_from_file(std::vector<char*>& reason_name2id,  std::vector<int>
 	compsky::mysql::query_buffer(&RES1,  "SELECT data FROM longstrings WHERE name='cmnt_body_regex'");
 	char* regexpr_str;
 	while(compsky::mysql::assign_next_row(RES1, &ROW1, &regexpr_str)){
-		// Only one iteration
+		// Only one iteration - the next call to assign_next_row frees the memory
 		compsky::regex::convert_named_groups(regexpr_str,  regexpr_str,  reason_name2id,  groupindx2reason,  record_contents);
-		// Add one to the first buffer (src) not second buffer (dst) to ensure it is never overwritten when writing dst
 
 		regexpr = new boost::basic_regex<char, boost::cpp_regex_traits<char>>(regexpr_str,  boost::regex::perl | boost::regex::optimize);
 	}
