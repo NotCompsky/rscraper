ba4341b5128219a651e17408273c0926c26712c6
NotCompsky <adammarkgray@gmail.com>
2020-08-04 13:00:54 +0100

    Update/Tidy/Fix		Complete the move of the regex_editor to Egix project.

--- hub/src/regex_editor/editor.hpp
+++ hub/src/regex_editor/editor.hpp
@@ -5,39 +5,21 @@
  *     This copyright notice must be included at the beginning of any copied/modified file originating from this project, or at the beginning of any section of code that originates from this project.
  */
 
-#ifndef RSCRAPER_HUB_REGEX_EDITOR_HPP
-#define RSCRAPER_HUB_REGEX_EDITOR_HPP
+#pragma once
 
-#include <QCheckBox>
-#include <QDialog>
+#include <egix/editor.hpp>
 
 
-class CodeEditor;
-class RegexEditorVarsMenu;
-
-
-class RegexEditor : public QDialog {
+class RScraperRegexEditor : public RegexEditor {
   public:
-	RegexEditor(const char* srcvar,  const char* dstvar,  QWidget* parent = 0);
-  private Q_SLOTS:
-	void test_regex();
-	void save_to_file();
+	RScraperRegexEditor(const char* srcvar,  const char* dstvar,  QWidget* parent = 0)
+	: src(srcvar)
+	, dst(dstvar)
+	, RegexEditor(parent)
+	{}
   private:
-	void find_text();
-	void ensure_buf_sized(const size_t buf_sz);
-	bool does_user_want_optimisations() const;
-	char* buf;
-	char* itr;
-	int buf_sz; // int, rather than size_t, because that is what Qt uses
-	bool to_final_format(const bool optimise,  QString& buf,  int i = 0,  int j = 1,  int last_optimised_group_indx = 0,  int var_depth = 0);
-	void display_help() const;
-	void load_file();
-	QCheckBox* want_optimisations;
-	RegexEditorVarsMenu* vars_menu;
 	const char* src;
 	const char* dst;
-	CodeEditor* text_editor;
+	void load_file();
+	void save_to_file();
 };
-
-
-#endif
